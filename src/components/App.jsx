741504eb940247e15756955bb07dd772f80631f4
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-08-22 10:55:00 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react';
 import { NavLink, Navigate, Route, Routes } from 'react-router-dom';
+
 import Loader from './Loader/Loader';
 import css from '../pages/HomePage/HomePage.module.css';
 
